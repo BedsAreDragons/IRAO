6ee82b28f97a2d9ca7f3f8d83309c722b813a2d4
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-04-29 11:29:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/org')
+def org():
+    return render_template('342698.pdf')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
